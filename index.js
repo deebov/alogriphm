e79947f7422cf046548b119e728ce1a08f72cbdd
Dilshod Turobov <deebov@yandex.com>
2018-06-26 11:21:29 +0500

    added the fibMemo() method

--- index.js
+++ index.js
@@ -146,6 +146,25 @@ class Alogriphm {
       return fibonacci(position - 1) + fibonacci(position - 2);
     }
   }
+
+  fibMemo(index, cache = []) {
+    this.is(index, 'number');
+    this.is(cache, 'array');
+    
+    if(cache[index]) {
+      return cache[index];
+    }
+    else {
+      if (index < 3) {
+        return 1;
+      }
+      else {
+        cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+      }
+    }
+    
+    return cache[index];
+  }
 }
 
 
