464b482b0a8e7a9f0e2f7e1180fbe462cf7a7daf
Dilshod Turobov <deebov@yandex.com>
2018-06-26 11:18:32 +0500

    added the fibonacci() method

--- index.js
+++ index.js
@@ -136,6 +136,16 @@ class Alogriphm {
     }
   }
 
+  fibonacci(position) {
+    this.is(position, 'number');
+
+    if(position < 3) {
+      return 1;
+    }
+    else {
+      return fibonacci(position - 1) + fibonacci(position - 2);
+    }
+  }
 }
 
 
