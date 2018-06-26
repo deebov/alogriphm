5295037fee06b5b6db24e096aa78562be742e7c9
Dilshod Turobov <deebov@yandex.com>
2018-06-26 11:15:41 +0500

    added the fizzBuzz() method

--- index.js
+++ index.js
@@ -45,6 +45,22 @@ class Alogriphm {
     return array;
   }
 
+  fizzBuzz(number) {
+    this.is(number, 'number', 'The argument must be number');
+
+    for(let i = 1; i <= number; i++) {
+      if(i % 15 === 0) {
+        console.log('FizzBuzz');
+      } else if(i % 5 === 0) {
+        console.log('buzz');
+      } else if(i % 3 === 0){
+        console.log('fizz');
+      } else {
+        console.log(i);
+      }
+    }
+  }
+
 }
 
 
