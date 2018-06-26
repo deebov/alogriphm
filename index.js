71100809db2b5fd9acef08100a345a43f7425910
Dilshod Turobov <deebov@yandex.com>
2018-06-26 11:14:43 +0500

    added the replaceArrayInPlace() method

--- index.js
+++ index.js
@@ -32,7 +32,19 @@ class Alogriphm {
 
     return reversedWords.join(' ');
   }
+
+  reverseArrayInPlace(array) {
+    this.is(array, 'array', 'The argument must be array');
+
+    for(let i = 0; i < array.length / 2; i++) {
+      let tempVar = array[i];
+      array[i] = array[array.length - 1 - i];
+      array[array.length - 1 - i] = tempVar;
+    }
   
+    return array;
+  }
+
 }
 
 
